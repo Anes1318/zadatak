8f5c0765001aee7500e51f65df891b9ba2c26cd4
Anes <anocoka@gmail.com>
2022-06-19 21:21:11 +0200

    logout pravim2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Skoro pa dobar sajt') }}</title>
+    <title>Skoro pa dobar sajt</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
